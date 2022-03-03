98b899976542c693251384de0aac9bcf586f60d1
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2022-03-02 23:52:47 -0300

    chore: Adds sessions and constants

---
+++ app/SessionState.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from typing import Any
+
+
+class SessionState:
+    DEFAULT_VALUES = {
+        'queue': [],
+        'message_feedback': None,
+    }
+
+    @staticmethod
+    def clear(key: str):
+        SessionState._check_key(key)
+        st.session_state[key] = SessionState.DEFAULT_VALUES[key]
+
+    @staticmethod
+    def get(key: str) -> Any:
+        SessionState._check_key(key)
+        SessionState._set_default_value_if_necessary(key)
+        return st.session_state[key]
+
+    @staticmethod
+    def set(key: str, value: Any):
+        SessionState._check_key(key)
+        st.session_state[key] = value
+
+    @staticmethod
+    def _set_default_value_if_necessary(key: str):
+        if key not in st.session_state:
+            st.session_state[key] = SessionState.DEFAULT_VALUES[key]
+
+    @staticmethod
+    def _check_key(key: str):
+        if key not in SessionState.DEFAULT_VALUES.keys():
+            raise ValueError('Invalid key value.')
---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from .constants import LADDER_MAPS, RACES, DIFFICULTY, PRIORITY
+from .SessionState import SessionState
+
+__all__ = [
+    'LADDER_MAPS',
+    'RACES',
+    'DIFFICULTY',
+    'PRIORITY',
+    'SessionState',
+]
